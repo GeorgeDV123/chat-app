a363fd0e5a9582c2d1db434c51c9cedc9b4e7bd8
GeorgeDV123 <115605263+GeorgeDV123@users.noreply.github.com>
2023-01-09 14:10:25 +1100

    Basic functionality done

---
+++ src/components/SignInOut.jsx
@@ -0,0 +1,35 @@
+import "firebase/compat/analytics";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+export function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>
+        Sign in with Google
+      </button>
+      <p>
+        Do not violate the community guidelines or you will be banned for life!
+      </p>
+    </>
+  );
+}
+
+export function SignOut() {
+  return (
+    auth.currentUser && (
+      <button className="sign-out" onClick={() => auth.signOut()}>
+        Sign Out
+      </button>
+    )
+  );
+}
