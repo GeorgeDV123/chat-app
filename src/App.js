49c14ff961dbdc4ed2ed53443154a1799f1e7a28
GeorgeDV123 <115605263+GeorgeDV123@users.noreply.github.com>
2023-01-09 12:03:13 +1100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useRef, useState } from 'react';
+import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/analytics';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+function App() {
+
+  return (
+    <div className="App">
+
+    </div>
+  )
+}
+
+export default App
