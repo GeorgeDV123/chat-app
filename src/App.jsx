850de250b42cc4397d1df5ddeee5b0c226246355
GeorgeDV123 <115605263+GeorgeDV123@users.noreply.github.com>
2023-01-11 10:56:27 +1100

    Curvy bg done

--- src/App.jsx
+++ src/App.jsx
@@ -1,139 +1,54 @@
-import { React, useRef, useState } from "react";
-
-import "firebase/compat/analytics";
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import "firebase/compat/firestore";
-
-import { useAuthState } from "react-firebase-hooks/auth";
-import { useCollectionData } from "react-firebase-hooks/firestore";
-
-import "/style.css";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDExTGrAXK0KrDGCVfBj9PZzBrUemegq1U",
-  authDomain: "chat-app-164e0.firebaseapp.com",
-  projectId: "chat-app-164e0",
-  storageBucket: "chat-app-164e0.appspot.com",
-  messagingSenderId: "1072635573296",
-  appId: "1:1072635573296:web:d0df3d49f3c482c3dd69f5",
-  measurementId: "G-983H8L01ZW",
-};
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-
-const auth = firebase.auth();
-const firestore = firebase.firestore();
+import React from "react";
+import "./components/css.css";
 
 function App() {
-  const [user] = useAuthState(auth);
-
-  return (
-    <div className="App">
-      <header>
-        <h1>⚛️🔥💬</h1>
-        <h2>Hi</h2>
-        <SignOut />
-      </header>
-
-      <section className="chat-1">{user ? <ChatRoom /> : <SignIn />}</section>
-    </div>
-  );
-}
-
-function SignIn() {
-  const signInWithGoogle = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider);
-  };
-
   return (
     <>
-      <button className="sign-in" onClick={signInWithGoogle}>
-        Sign in with Google
-      </button>
-      <button className="sign-inF">Sign in as Guest</button>
-      <p>
-        Do not violate the community guidelines or you will be banned for life!
-      </p>
-    </>
-  );
-}
+      <section className="spacer layer2 ">
+        <h2>Nice Curves</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque
+          dolorem iste dolores qui voluptatibus autem excepturi, impedit minus
+          quae debitis accusamus error ut labore maxime! Nemo pariatur veritatis
+          accusantium voluptates!
+        </p>
+      </section>
 
-function SignOut() {
-  return (
-    auth.currentUser && (
-      <button className="sign-out" onClick={() => auth.signOut()}>
-        Sign Out
-      </button>
-    )
-  );
-}
+      <section>
+        <h2>Nice Curves</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque
+          dolorem iste dolores qui voluptatibus autem excepturi, impedit minus
+          quae debitis accusamus error ut labore maxime! Nemo pariatur veritatis
+          accusantium voluptates!
+        </p>
+      </section>
 
-function ChatRoom() {
-  const dummy = useRef();
-  const messagesRef = firestore.collection("messages");
-  const query = messagesRef.orderBy("createdAt").limit(25);
+      <section className="spacer layer1">
+        <div className="curve">
+          <img src="/imgs/dark-waves.svg" />
+        </div>
+        <h2>Nice Curves</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque
+          dolorem iste dolores qui voluptatibus autem excepturi, impedit minus
+          quae debitis accusamus error ut labore maxime! Nemo pariatur veritatis
+          accusantium voluptates!
+        </p>
+      </section>
 
-  const [messages] = useCollectionData(query, { idField: "id" });
-
-  const [formValue, setFormValue] = useState("");
-
-  const sendMessage = async (e) => {
-    e.preventDefault();
-
-    const { uid, photoURL } = auth.currentUser;
-
-    await messagesRef.add({
-      text: formValue,
-      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-      uid,
-      photoURL,
-    });
-
-    setFormValue("");
-    dummy.current.scrollIntoView({ behavior: "smooth" });
-  };
-
-  return (
-    <>
-      <main>
-        {messages &&
-          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
-
-        <span ref={dummy}></span>
-      </main>
-
-      <form onSubmit={sendMessage}>
-        <input
-          value={formValue}
-          onChange={(e) => setFormValue(e.target.value)}
-          placeholder="say something nice"
-        />
-
-        <button type="submit" disabled={!formValue}>
-          🕊️
-        </button>
-      </form>
-    </>
-  );
-}
-
-function ChatMessage(props) {
-  const { text, uid, photoURL } = props.message;
-
-  const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
-
-  return (
-    <>
-      <div className={`message ${messageClass}`}>
-        <img
-          src={
-            photoURL || "https://api.adorable.io/avatars/23/abott@adorable.png"
-          }
-        />
-        <p>{text}</p>
-      </div>
+      <section className="spacer layer3">
+        <div className="tilt ">
+          <img src="/imgs/tilt.svg" />
+        </div>
+        <h2>Nice Curves</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque
+          dolorem iste dolores qui voluptatibus autem excepturi, impedit minus
+          quae debitis accusamus error ut labore maxime! Nemo pariatur veritatis
+          accusantium voluptates!
+        </p>
+      </section>
     </>
   );
 }
